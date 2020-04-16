6f46a6305e43fe041239b383ec8bcab31033a936
daniel mcnamara <danmac8407@gmail.com>
2020-04-16 18:02:32 -0500

    mvp

---
+++ data/db-config.js
@@ -0,0 +1,9 @@
+const knex = require("knex");
+
+const config = require("../knexfile");
+
+// const environment = process.env.DB_ENV || "development";
+
+module.exports = knex(
+  config.development //[environment]
+);
